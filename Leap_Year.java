17d6c98d02acdff2a72f35ab2b9c169d86b6e50b
pranaybhujade <pranaybhujade@gmail.com>
2022-08-27 13:08:21 +0530

    leap year

---
+++ Leap_Year.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.basic;
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+        int year = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter year : ");
+
+        year = scanner.nextInt();
+        while (year < 999 || year > 9999) {
+            System.out.print("Opps...year is not valid \nEnter 4 digit number for year : ");
+            year = scanner.nextInt();
+        }
+
+        if ((year % 2) == 0) {
+            System.out.println(year + " is leap year");
+        }
+        else
+        {
+            System.out.println(year + " is not leap year");
+        }
+    }
+}
+
