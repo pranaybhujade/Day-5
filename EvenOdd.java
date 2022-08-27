23efae5bd1efc28c7f3975482b84e2c33c20cfe6
pranaybhujade <pranaybhujade@gmail.com>
2022-08-27 12:40:14 +0530

    even odd

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.basic;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int t;
+        t = a;
+        a = b;
+        b = t;
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
+    }
+}
