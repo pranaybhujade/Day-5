4e7d5ead6286083228667fb6d3b8371bb170d739
pranaybhujade <pranaybhujade@gmail.com>
2022-08-27 13:05:01 +0530

    greater of three

---
+++ LargerThree.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.basic;
+
+public class LargerThree {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        if (a > b && a > c) {
+            System.out.println("a is greater");
+        } else if (b > c && b > c) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("c is greater");
+        }
+    }
+}
